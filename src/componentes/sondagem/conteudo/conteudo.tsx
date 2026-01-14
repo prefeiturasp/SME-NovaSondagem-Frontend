d839b6e6cb9ad95b7416c17c84791afb2cea736f
Felipe Piorini <felipemolica@hotmail.com>
2026-01-14 14:19:59 -0300

    fix: Ajustes sonar

--- src/componentes/sondagem/conteudo/conteudo.tsx
+++ src/componentes/sondagem/conteudo/conteudo.tsx
@@ -283,8 +283,8 @@ const Conteudo: React.FC = () => {
       console.error("ERRO:", error);
 
       const errorMessage =
-        error.response?.data?.title ||
-        error.response?.data?.message ||
+        error.response?.data?.title ??
+        error.response?.data?.message ??
         "Erro ao salvar a sondagem. Tente novamente.";
 
       const errorDetails = error.response?.data?.errors
@@ -298,7 +298,7 @@ const Conteudo: React.FC = () => {
 
       notification.error({
         message: "Erro ao salvar sondagem",
-        description: errorDetails || errorMessage,
+        description: errorDetails ?? errorMessage,
         duration: 5,
         placement: "topRight",
       });
