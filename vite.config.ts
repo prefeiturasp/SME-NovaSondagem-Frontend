172a88a4b06e9924fdc3c6419bbf0c0c6998da82
Bruna Lima <bruna.lima9254@gmail.com>
2026-01-14 22:14:54 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,66 @@ import federation from "@originjs/vite-plugin-federation";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
+      "~": path.resolve(__dirname, "./src"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    watch: {
-      usePolling: true,
+    port: 5173,
+    cors: false,
+  },
+  preview: {
+    port: 5173,
+    cors: false,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "smeNovaSondagem",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Home": "./src/paginas/home/home.tsx",
+      },
+      shared: {
+        react: {
+          requiredVersion: "^18.2.0",
+        },
+        "react-dom": {
+          requiredVersion: "^18.2.0",
+        },
+        "react-redux": {
+          requiredVersion: "^8.1.3",
+        },
+        antd: {
+          requiredVersion: "^5.4.0",
+        },
+      },
+    }),
+    {
+      name: "module-federation-debug",
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.includes("remoteEntry.js")) {
+            res.setHeader("Content-Type", "application/javascript");
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "*");
+            res.setHeader("Access-Control-Allow-Headers", "*");
+            res.setHeader("Access-Control-Expose-Headers", "*");
+          }
+          next();
+        });
+      },
+    },
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      external: ["react-redux"],
     },
   },
 });
